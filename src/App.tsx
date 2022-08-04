6485b00b5b38f9a2c9a3ac54afd736f4c1985bf2
oscargelin <oscargelin@gmail.com>
2022-08-04 10:42:35 +0200

    Starting a react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+let temp = 123;
+let tempTwo = "123";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{temp}</h1>
     </div>
   );
 }
